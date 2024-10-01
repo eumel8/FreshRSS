679696118305d88c8a1e708184c52b5a596a6595
Frank Kloeker <frank.kloeker@telekom.de>
2024-10-01 21:23:47 +0200

    remove install and user creating stuff

--- Docker/entrypoint-k8s.sh
+++ Docker/entrypoint-k8s.sh
@@ -6,43 +6,17 @@ export OIDC_SESSION_TYPE="${OIDC_SESSION_TYPE:-server-cache}"
 OIDC_SCOPES=$(echo "$OIDC_SCOPES" | tr ':' ' ')
 export OIDC_SCOPES
 
+if [ ! -d '/var/www/FreshRSS' ]; then
+        echo >&2 '⛔ It does not look like a FreshRSS directory; exiting!'
+        exit 2
+fi
 
-if [ -n "$FRESHRSS_INSTALL" ]; then
+
+if [ ! -f '/var/www/FreshRSS/constants.php' ]; then
 	cp -r /var/www/FreshRSSORIG/ /var/www/FreshRSS
-	/var/www/FreshRSS/cli/access-permissions.sh
+	mkdir -p /var/www/FreshRSS/data/users/_/
+	chmod -R g+w /var/www/FreshRSS/data/
 	php -f /var/www/FreshRSS/cli/prepare.php >/dev/null
-	# shellcheck disable=SC2046
-	php -f /var/www/FreshRSS/cli/do-install.php -- \
-		$(echo "$FRESHRSS_INSTALL" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
-	EXITCODE=$?
-
-	if [ $EXITCODE -eq 3 ]; then
-		echo 'ℹ️ FreshRSS already installed; no change performed.'
-	elif [ $EXITCODE -eq 0 ]; then
-		echo '✅ FreshRSS successfully installed.'
-	else
-		echo '❌ FreshRSS error during installation!'
-		exit $EXITCODE
-	fi
 fi
-
-if [ -n "$FRESHRSS_USER" ]; then
-	# shellcheck disable=SC2046
-	php -f /var/www/FreshRSS/cli/create-user.php -- \
-		$(echo "$FRESHRSS_USER" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
-	EXITCODE=$?
-
-	if [ $EXITCODE -eq 3 ]; then
-		echo 'ℹ️ FreshRSS user already exists; no change performed.'
-	elif [ $EXITCODE -eq 0 ]; then
-		echo '✅ FreshRSS user successfully created.'
-		/var/www/FreshRSS/cli/list-users.php | xargs -n1 /var/www/FreshRSS/cli/actualize-user.php --user
-	else
-		echo '❌ FreshRSS error during the creation of a user!'
-		exit $EXITCODE
-	fi
-fi
-
-/var/www/FreshRSS/cli/access-permissions.sh
 
 exec "$@"
