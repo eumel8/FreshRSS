53ed9e0b355b85a216af01ba12424d7312f53f71
Frank Kloeker <frank.kloeker@telekom.de>
2024-10-01 13:15:52 +0200

    add k8s image and build workflow

---
+++ Docker/entrypoint-k8s.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+	export OIDC_SESSION_INACTIVITY_TIMEOUT="${OIDC_SESSION_INACTIVITY_TIMEOUT:-300}"
+	export OIDC_SESSION_MAX_DURATION="${OIDC_SESSION_MAX_DURATION:-27200}"
+	export OIDC_SESSION_TYPE="${OIDC_SESSION_TYPE:-server-cache}"
+	OIDC_SCOPES=$(echo "$OIDC_SCOPES" | tr ':' ' ')
+	export OIDC_SCOPES
+
+./cli/access-permissions.sh
+
+php -f ./cli/prepare.php >/dev/null
+
+if [ -n "$FRESHRSS_INSTALL" ]; then
+	# shellcheck disable=SC2046
+	php -f ./cli/do-install.php -- \
+		$(echo "$FRESHRSS_INSTALL" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
+	EXITCODE=$?
+
+	if [ $EXITCODE -eq 3 ]; then
+		echo 'ℹ️ FreshRSS already installed; no change performed.'
+	elif [ $EXITCODE -eq 0 ]; then
+		echo '✅ FreshRSS successfully installed.'
+	else
+		echo '❌ FreshRSS error during installation!'
+		exit $EXITCODE
+	fi
+fi
+
+if [ -n "$FRESHRSS_USER" ]; then
+	# shellcheck disable=SC2046
+	php -f ./cli/create-user.php -- \
+		$(echo "$FRESHRSS_USER" | sed -r 's/[\r\n]+/\n/g' | paste -s -)
+	EXITCODE=$?
+
+	if [ $EXITCODE -eq 3 ]; then
+		echo 'ℹ️ FreshRSS user already exists; no change performed.'
+	elif [ $EXITCODE -eq 0 ]; then
+		echo '✅ FreshRSS user successfully created.'
+		./cli/list-users.php | xargs -n1 ./cli/actualize-user.php --user
+	else
+		echo '❌ FreshRSS error during the creation of a user!'
+		exit $EXITCODE
+	fi
+fi
+
+./cli/access-permissions.sh
+
+exec "$@"
